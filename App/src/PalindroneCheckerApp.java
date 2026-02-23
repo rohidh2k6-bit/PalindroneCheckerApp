a2b212ddff19e496477cfa82dc27c97ccee0f6de
shiva-srm <shi1952006@gmail.com>
2026-02-23 09:22:24 +0530

    Rohidh Added UC2 HardCode Palindrome

--- App/src/PalindroneCheckerApp.java
+++ App/src/PalindroneCheckerApp.java
@@ -1,5 +1,6 @@
 public class PalindroneCheckerApp {
     public static void main(String[] args){
         System.out.println("welcome to palindrome checker app managemnet system");
+        System.out.println("modify this logic to usecase 2");
     }
 }
